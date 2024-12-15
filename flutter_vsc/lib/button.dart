0ab01108ce6224f40a6558baf0cc17296cc6696e
huyne265 <123146801+huyne265@users.noreply.github.com>
2024-12-15 17:09:29 +0700

    update mode relay

--- flutter_vsc/lib/button.dart
+++ flutter_vsc/lib/button.dart
@@ -20,6 +20,7 @@ class _RelayControlPageState extends State<RelayControlPage> {
   bool relay3 = false;
   bool relay4 = false;
 
+  bool isManualMode = false;
   double lightValue = 0.0;
 
   @override
@@ -32,7 +33,6 @@ class _RelayControlPageState extends State<RelayControlPage> {
       final data = event.snapshot.value;
       if (data != null) {
         double newLightValue = double.tryParse(data.toString()) ?? 0.0;
-
         _updateLightValue(newLightValue);
       }
     });
@@ -45,13 +45,19 @@ class _RelayControlPageState extends State<RelayControlPage> {
         bool newRelay3 = data['Relay3'] == 1;
         bool newRelay4 = data['Relay4'] == 1;
         setState(() {
-          if (relay1 != newRelay1) relay1 = newRelay1;
-          if (relay2 != newRelay2) relay2 = newRelay2;
-          if (relay3 != newRelay3) relay3 = newRelay3;
-          if (relay4 != newRelay4) relay4 = newRelay4;
+          relay1 = newRelay1;
+          relay2 = newRelay2;
+          relay3 = newRelay3;
+          relay4 = newRelay4;
         });
       }
     });
+
+    _updateModeInFirebase();
+  }
+
+  void _updateModeInFirebase() {
+    databaseReference.child('Relay/Mode').set(isManualMode ? 'Manual' : 'Auto');
   }
 
   void _updateLightValue(double newLightValue) {
@@ -59,29 +65,33 @@ class _RelayControlPageState extends State<RelayControlPage> {
       lightValue = newLightValue;
     });
 
-    //Relay1 - light
-    if (lightValue < 6 && !relay1) {
-      setState(() {
-        relay1 = true;
-      });
-      _toggleRelay('Relay1', true);
-    } else if (lightValue > 90 && relay1) {
-      setState(() {
-        relay1 = false;
-      });
-      _toggleRelay('Relay1', false);
+    if (!isManualMode) {
+      //Relay1 - light
+      if (lightValue < 6 && !relay1) {
+        setState(() {
+          relay1 = true;
+        });
+        _toggleRelay('Relay1', true);
+      } else if (lightValue > 90 && relay1) {
+        setState(() {
+          relay1 = false;
+        });
+        _toggleRelay('Relay1', false);
+      }
     }
   }
 
   void _toggleRelay(String relayKey, bool status) {
-    databaseReference.child('Relay').update({
-      relayKey: status ? 1 : 0,
-    });
+    if (isManualMode) {
+      databaseReference.child('Relay').update({
+        relayKey: status ? 1 : 0,
+      });
+    }
   }
 
   Widget _buildRelayCard(String name, bool status, VoidCallback onPressed) {
     return GestureDetector(
-      onTap: onPressed,
+      onTap: isManualMode ? onPressed : null,
       child: Container(
         height: 120,
         width: 120,
@@ -150,27 +160,73 @@ class _RelayControlPageState extends State<RelayControlPage> {
           ),
         ),
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Wrap(
-              spacing: 20,
-              runSpacing: 20,
-              alignment: WrapAlignment.center,
-              children: [
-                _buildRelayCard("Relay 1", relay1, () {
-                  _toggleRelay('Relay1', !relay1);
-                }),
-                _buildRelayCard("Relay 2", relay2, () {
-                  _toggleRelay('Relay2', !relay2);
-                }),
-                _buildRelayCard("Relay 3", relay3, () {
-                  _toggleRelay('Relay3', !relay3);
-                }),
-                _buildRelayCard("Relay 4", relay4, () {
-                  _toggleRelay('Relay4', !relay4);
-                }),
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF7da0ca),
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(4, 4),
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.settings, color: Colors.white),
+                    const SizedBox(width: 10),
+                    Text(
+                      isManualMode ? 'Mode: Manual' : 'Mode: Automatic',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Switch(
+                      value: isManualMode,
+                      onChanged: (bool value) {
+                        setState(() {
+                          isManualMode = value;
+                          _updateModeInFirebase();
+                        });
+                      },
+                      activeColor: const Color(0xFF2b669c),
+                      inactiveThumbColor: const Color(0xFF5483b3),
+                      inactiveTrackColor: const Color(0xFF7da0ca),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Wrap(
+                  spacing: 20,
+                  runSpacing: 20,
+                  alignment: WrapAlignment.center,
+                  children: [
+                    _buildRelayCard("Relay 1", relay1, () {
+                      _toggleRelay('Relay1', !relay1);
+                    }),
+                    _buildRelayCard("Relay 2", relay2, () {
+                      _toggleRelay('Relay2', !relay2);
+                    }),
+                    _buildRelayCard("Relay 3", relay3, () {
+                      _toggleRelay('Relay3', !relay3);
+                    }),
+                    _buildRelayCard("Relay 4", relay4, () {
+                      _toggleRelay('Relay4', !relay4);
+                    }),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
